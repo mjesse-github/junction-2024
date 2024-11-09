72f9aaa0a421c5a6e5dcc9d32ec4ddad59da1ddc
Denys Kovalenko <denys.kovalenko@bolt.eu>
2024-11-09 15:21:58 +0200

    try fix gh action

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/your-repo-name',
+  basePath: 'junction-2024',
   images: {
     unoptimized: true,
   },
